3eb5392c430ac4d481967d6ae6d8e8954f2f49aa
AndreaVanElmpt <oibibio@gmail.com>
2022-05-11 10:00:31 +0200

    eerste update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <h1>Fruitmand bezorgservice</h1>
-    </>
-  );
+    const [counter, setCounter] =useState(0)
+    return (
+        <>
+            <h1>Fruitmand bezorgservice</h1>
+            <article>
+                <h2>Aardbeien</h2>
+                <button onClick={() => setCounter(counter -1)}>
+                    -
+                </button>
+                {counter}
+                <button onClick={() => setCounter(counter +1)}>
+                    +
+                </button>
+            </article>
+        </>
+    );
 }
 
 export default App;
